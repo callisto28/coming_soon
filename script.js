e0c6719434f0cbc6503df4f1b8f5867c38af144c
callisto28 <iotaseb@gmail.com>
2021-09-26 16:16:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const textWrapper = document.querySelector('.ml12');
+const textWrapper2 = document.querySelector('.text');
+
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+
+  anime.timeline({loop: true})
+  .add({
+    targets: '.text .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 3000,
+    delay: (el, i) => 1500 + 30 * i
+  }).add({
+    targets: '.text .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 3100,
+    delay: (el, i) => 1100 + 30 * i
+  });
